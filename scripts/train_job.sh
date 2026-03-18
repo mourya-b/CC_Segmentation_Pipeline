d3933b35050aedc1f1c1794fcd0f18c12e9055e0
mourya-b <mourya.bandaru05@gmail.com>
2026-03-18 12:30:08 +0100

    Fix 2 for sbatch

--- scripts/train_job.sh
+++ scripts/train_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --container-image="dockerdex.umcn.nl:5005#mourya-b/cc_segmentation_pipeline/cc_segmentation_pipeline:v1.0"
+#SBATCH --container-image="dockerdex.umcn.nl:5005#mourya-b/cc_segmentation_pipeline:v1.0"
 #SBATCH --container-mounts="/data/diag:/data/diag"
 #SBATCH --output=/data/diag/mouryaBandaru/experiments/classifier_v1/logs/%j.out
 #SBATCH --error=/data/diag/mouryaBandaru/experiments/classifier_v1/logs/%j.err
