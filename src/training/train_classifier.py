87401bff00951e4b2bdddee2e39b45d79acff1d6
mourya-b <mourya.bandaru05@gmail.com>
2026-05-15 17:47:12 +0200

    v10: remove GaussNoise, add ElasticTransform, reduce BrightnessContrast

--- src/training/train_classifier.py
+++ src/training/train_classifier.py
@@ -51,8 +51,8 @@ def get_transforms(train=True, image_size=512):
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
             A.RandomRotate90(p=0.5),
-            A.RandomBrightnessContrast(p=0.3),
-            A.GaussNoise(p=0.2),
+            A.RandomBrightnessContrast(brightness_limit=0.15, contrast_limit=0.15, p=0.3),
+            A.ElasticTransform(alpha=1, sigma=10, p=0.3),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
         ])
