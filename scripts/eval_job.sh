054103e7f0991511e2ed71f901c54e8b387803e3
mourya-b <mourya.bandaru05@gmail.com>
2026-05-18 17:37:30 +0200

    eval_job updatedpath

--- scripts/eval_job.sh
+++ scripts/eval_job.sh
@@ -9,10 +9,10 @@
 #SBATCH --container-image="dockerdex.umcn.nl:5005#mourya-b/cc_segmentation_pipeline:v1.2"
 #SBATCH --container-mounts="/data/diag:/data/diag"
 #SBATCH --container-workdir="/data/diag/mouryaBandaru/CC_Segmentation_Pipeline"
-#SBATCH --output=/data/diag/mouryaBandaru/experiments/classifier_v4/logs/eval_%j.out
-#SBATCH --error=/data/diag/mouryaBandaru/experiments/classifier_v4/logs/eval_%j.err
+#SBATCH --output=/data/diag/mouryaBandaru/experiments/classifier_v16/logs/eval_%j.out
+#SBATCH --error=/data/diag/mouryaBandaru/experiments/classifier_v16/logs/eval_%j.err
 
 export PYTHONPATH=/data/diag/mouryaBandaru/CC_Segmentation_Pipeline
 python -m src.evaluation.evaluate_classifier \
   --config configs/train_classifier_cluster.yaml \
-  --checkpoint /data/diag/mouryaBandaru/experiments/classifier_v4/best_classifier.pth
+  --checkpoint /data/diag/mouryaBandaru/experiments/classifier_v16/best_classifier.pth
